448aa18a6680768ad49a1c6b63dcb9a478d8e390
christianjayalipio-bootcamp <christian.jay.alipio@accenture.com>
2025-08-12 18:48:48 +0800

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["com/ui5/trng/project2/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"com/ui5/trng/project2/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
